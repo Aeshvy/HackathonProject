009655c7d244bc406103d147927c6394c6d47c2a
Alex <aeshvy@gmail.com>
2025-04-22 15:52:21 +0300

    Game logic

--- game/hangman.py
+++ game/hangman.py
@@ -2,3 +2,22 @@ import random
 
 LIVES = 6
 
+class hangmanGame:
+    def __init__(self, word):
+        self.word = word.upper()
+        self.guessed_letters = set()
+        self.attempts_left = LIVES
+
+    def guess(self, letter):
+        letter = letter.upper()
+        if letter in self.guessed_letters:
+            return "Already guessed."
+        self.guessed_letters.add(letter)
+
+        if letter in self.word:
+            return "Correct!"
+        else:
+            self.attempts_left -= 1
+            return "Wrong!"
+    
+    def
