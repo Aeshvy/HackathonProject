d7bd81d509bb1ba4df1e60f54283ea91bd4eb1cc
Alex <aeshvy@gmail.com>
2025-04-25 20:31:59 +0300

    Updated Faker Functionality

--- game/player.py
+++ game/player.py
@@ -6,11 +6,11 @@ fake = Faker()
 class Player:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.name = fake.first_name()
+        self.name = f"The {fake.color_name()} {fake.word().capitalize()}"
         self.score = 0
 
     def increment_score(self):
         self.score += 1
 
-    def __repr(self):
+    def __repr__(self):
         return f"{self.name} (Score: {self.score})"
